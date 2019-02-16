faf7f3cd163df3db84f19e23a2750607a364d778
SabihaDjurina <sabiha.djurina@abv.bg>
2019-02-16 23:08:55 +0200

    json file merge conflict fixed

--- src/NineGagProject/Demo.java
+++ src/NineGagProject/Demo.java
@@ -19,72 +19,34 @@ public class Demo {
 	
 	public static void main(String[] args) throws InvalidDataException, InterruptedException, NotLoggedInException, InvalidSectionException, IOException {
 		
-
-//		Ads ad = new Ads();
-//		ad.start();
-		
-		User usi = new User("Ivan m","Cska1948","ivan@abv.bg");
-		
-		
-			Post post1 = usi.createAPost("photo1", "Mega qkata snimka, brat!", "Animals", false);
-					post1.addTagsToPost("snimka","qko","fun");
-			usi.createAPost("photo2", "Malko tupa snimka, brat!", "Funny", true).addTagsToPost("tag1","tag10");
-		
-			
-		
-
-		
-		
-		User usi2 = new User("Ivan m2","Cska19482","ivan2@abv.bg");
-		Post post2 = usi2.createAPost("photo12", "Mega qkata snimka, brat!22", "Animals", false);
-		post1.addTagsToPost("snimka22","qko22","fun22");
-		User usi3 = new User("Ivan m3","Cska19483","ivan3@abv.bg");
-		
-		
-		
-//		UserStorage storage = UserStorage.giveUserStorage();
-//		storage.addUserToSite(usi);
-//		storage.addUserToSite(usi2);
-//		storage.addUserToSite(usi3);
-		
-		usi2.writeAComment(post1, "usi2 comment");
-		usi3.writeAComment(post1, "usi3 comment");
-		Comment c = usi.writeAComment(post2, "commenting this");
-		usi2.writeAReply(c, "this is my reply");
-		
-		
-		
-		usi.putNotifications();
-		usi.showMyNotifications();
-		
-//		storage.printAllUsers();
+//		User usi = new User("Ivan m","Cska1948","ivan@abv.bg");
 //		
-//		storage.toJson();
-		
-		
-//		storage.loadJson("src\\NineGagProject\\jsonStorage.json");
-//		storage.setUserNames();
-//		storage.printAllUsers();
-//		
-//		
-//		User usi2 = new User("Mitko Petrov","MitakaBaby23","mPetrov23@abv.bg");
+//		Post post1 = usi.createAPost("photo1", "Mega qkata snimka, brat!", "Animals", false);
+//		post1.addTagsToPost("snimka","qko","fun");
+//		usi.createAPost("photo2", "Malko tupa snimka, brat!", "Funny", true).addTagsToPost("tag1","tag10");
+//		User usi2 = new User("Ivan m2","Cska19482","ivan2@abv.bg");
+//		Post post2 = usi2.createAPost("photo12", "Mega qkata snimka, brat!22", "Animals", false);
+//		post1.addTagsToPost("snimka22","qko22","fun22");
 //		User usi3 = new User("Ivan m3","Cska19483","ivan3@abv.bg");
-
+//			
 		UserStorage storage = UserStorage.giveUserStorage();
 //		storage.addUserToSite(usi);
 //		storage.addUserToSite(usi2);
 //		storage.addUserToSite(usi3);
 //		
-//		storage.printAllUsers();		
-//		storage.toJson();	
-//		storage.loadJson("src\\NineGagProject\\jsonStorage.json");
-//		storage.setUserNames();
-//		storage.printAllUsers();
-		//storage.printCollection();
-		
-		
-		
+//		usi2.writeAComment(post1, "usi2 comment");
+//		usi3.writeAComment(post1, "usi3 comment");
+//		Comment c = usi.writeAComment(post2, "commenting this");
+//		usi2.writeAReply(c, "this is my reply");
 
+//		usi.putNotifications();
+//		usi.showMyNotifications();
+
+//		storage.toJson();
+		
+		storage.loadJson("src\\NineGagProject\\jsonStorage.json");
+		storage.setUserNames();
+		storage.printAllUsers();
 
 		
 		//usi.searching("tup");
@@ -93,10 +55,10 @@ public class Demo {
 //		usi.showMyFavouriteSections();
 //		
 //		
-		NineGag.giveNineGag();
-		MenuFor9gag m = new MenuFor9gag();
-		m.main();
-		
+//		NineGag.giveNineGag();
+//		MenuFor9gag m = new MenuFor9gag();
+//		m.main();
+//		
 		
 		
 //		//ninegag.showAllSections();
